969c165f819e1db8252b0c5ee9cbe9d7ce1aca46
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2025-07-18 04:27:12 +0300

    Update mongoDB.js

--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -1,16 +1,20 @@
 import mongoose from 'mongoose';
 import logger from '../utils/logger.js';
-import config from '../utils/config.js';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(config.dbURL); 
-    logger.info('MongoDB connected successfully');
+    // console.log('MONGO_URI:', process.env.MONGO_URI);
+      await mongoose.connect(process.env.MONGO_URI, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+        });
+     
+      logger.info('MongoDB connected successfully');
   } catch (error) {
-    logger.error(`Error: ${error.message}`);
-    process.exit(1);
+      logger.error(`Error: ${error.message}`);
+      process.exit(1); 
   }
 };
 
-export default connectDB;
 
+export default connectDB;
