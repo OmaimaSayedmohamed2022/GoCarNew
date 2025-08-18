fe1ea9556a1debd7af52ce6185e2f426072e1575
Your Name <you@example.com>
2025-08-19 02:26:48 +0300

    dashboard

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { summary,rideStatus ,recentEarnings} from "../controllers/dashboardController.js";
+import { summary,rideStatus ,recentEarnings, topDriversByEarning} from "../controllers/dashboardController.js";
 
 const router = express.Router();
 
 router.get("/summary", summary);
 router.get("/ride/Status",rideStatus)
-// router.get
+router.get("/recentEarnings",recentEarnings)
+router.get("/topDrivers",topDriversByEarning)
 
 
 
